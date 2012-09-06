b8ad319b14435c7279d084ca1fa43cfefc111b86
kevinality <kevinality.git@gmail.com>
2012-09-06 00:38:42 -0400

    Testing branching commits

--- ShittyPracticeGroup/ShittyPracticeGroup/ShittyPracticeGroup/Hero.cs
+++ ShittyPracticeGroup/ShittyPracticeGroup/ShittyPracticeGroup/Hero.cs
@@ -7,6 +7,9 @@ using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
 using Microsoft.Xna.Framework.Input;
 
+//This is just to test branching
+//This comment is useless.
+
 namespace ShittyPracticeGroup
 {
     class Hero
