3ffd24730e007c6a23ca4082aa840b15f9fa73f8
kevinality <kevinality.git@gmail.com>
2012-09-06 00:33:54 -0400

    Setup empty Inputhandler class

---
+++ ShittyPracticeGroup/ShittyPracticeGroup/ShittyPracticeGroup/InputHandler.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Microsoft.Xna;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Input;
+
+namespace ShittyPracticeGroup
+{
+	class InputHandler
+	{
+	//implementation
+	}
+}
